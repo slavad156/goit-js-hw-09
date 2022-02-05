1b2417f38f50127cbfd94729589ff0745a385635
Viacheslav Diukov <slavad156@ukr.net>
2022-02-05 22:22:00 +0200

    finishes task-03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,39 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector(`.form`);
+
+form.addEventListener(`submit`, formSubmit);
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  const formData = { position, delay };
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(formData);
+      } else {
+        reject(formData);
+      }
+    }, delay);
+  });
+}
+
+function formSubmit(evt) {
+  evt.preventDefault();
+
+  let delay = Number(evt.currentTarget.delay.value);
+  let step = Number(evt.currentTarget.step.value);
+  let amount = Number(evt.currentTarget.amount.value);
+
+  for (let position = 1; position <= amount; position += 1) {
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += step;
   }
 }
